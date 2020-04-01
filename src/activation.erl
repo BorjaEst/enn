38d4f2381c6d3ce25e922ae0cd97dd58a8a198d8
BorjaEst <boressan@outlook.com>
2020-04-01 18:14:39 +0200

    conditional beta deactivated for future review

--- src/activation.erl
+++ src/activation.erl
@@ -85,13 +85,19 @@ beta(Function, Error, Soma) ->
     ?LOG_ACTIVATION_BETA_RESULT(Function, Result),
     Result.
 
-apply_beta(Function, Error, Soma) when Error =< 0 ->
-    if
-        Soma =< 0 -> Error * dfun(Function, Soma);
-        true      -> Error
-    end;
-apply_beta(Function, Error, Soma) ->
-    - apply_beta(Function, -Error, -Soma).
+% apply_beta(Function, Error, Soma) when Error =< 0 ->
+%     if
+%         Soma =< 0 -> Error * dfun(Function, Soma);
+%         true      -> Error
+%     end;
+% apply_beta(Function, Error, Soma) ->
+%     - apply_beta(Function, -Error, -Soma).
+
+
+apply_beta(Function, Error, Soma)->
+    Error * dfun(Function, Soma).
+
+
 
 
 %%====================================================================
