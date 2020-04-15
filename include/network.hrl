7c9896e657160183a48c9dc89cc50a53f551faa4
BorjaEst <boressan@outlook.com>
2020-04-15 20:27:28 +0200

    Network structure redefined

--- include/network.hrl
+++ include/network.hrl
@@ -1,30 +1,29 @@
 %%%-------------------------------------------------------------------
-%% @doc Neural networks configuration.
+%% @doc Neural networks definition.
 %% @end
 %%%------------------------------------------------------------------
 
--type networks_pool() :: ets:tid().
--type  neurons_pool() :: ets:tid().
--type network_graph() :: digraph:graph().
--define(NN_POOL, nn_pool).
--define(NN_POOL_OPTIONS, [
-    named_table,        % As any process should be able to start a nn
-    public,             % Every element registers itself (so public)
-    set,                % The pool must be a set (no repeated values)
-    {keypos, #network.id}    % The key of the record must be the id
-]).
+-import_lib("neuron.hrl").
+-import_lib("cortex.hrl").
 
+-type network_id() :: {reference(), network}.
 -record(network, {
-    id         :: cortex_id(), % Network identifier
-    supervisor :: pid(),       % Pid of the supervisor
-    cortex     :: pid(),       % Pid of the cortex 
-    graph      :: network_graph(), % Mounted graph with network
-    pid_pool   :: neurons_pool()   % ETS table with neurons pid->id 
-    % neurons     :: integer(),
-    % links       :: integer(),
-    % forward_cycles  = 0 :: integer(), % Number of FP performed cycles
-    % backward_cycles = 0 :: integer(), % Number of BP performed cycles
-    % last_bperr = []  :: [float()]  % Last back propagation errors
+    id = {make_ref(), network} :: network_id(),
+    cn = #{} :: #{neuron_id() => connections()},   
+    recurrent = true :: boolean()
 }).
 -type network() :: #network{}.
+
+-record(connections, {
+    seq = {[],[]} :: {In :: [neuron_id()], Out :: [neuron_id()]},
+    rcc = {[],[]} :: {In :: [neuron_id()], Out :: [neuron_id()]}
+}).
+-type connections() :: #connections{}.
+
+-type link_id() :: {From :: neuron_id(), To :: neuron_id(), connection}.
+-record(link, {
+    id :: linkn_id(),
+    w = uninitialized :: weight()
+}).
+-type link() :: #link{}.
 
