606c67b655d9224f3d787b5a761cda3b88f3bae6
BorjaEst <boressan@outlook.com>
2020-04-15 18:33:54 +0200

    network erl renamed in netgraph

--- src/netgraph.erl
+++ src/netgraph.erl
@@ -12,7 +12,7 @@
 %%%       update its weight values {{FromID, ToID, connection}, W}.
 %%% @end
 %%%-------------------------------------------------------------------
--module(network).
+-module(netgraph).
 
 -export([new/0, new/1, delete/1, info/1]).
 
@@ -28,16 +28,16 @@
 -export([out_conn/2, in_conn/2]).
 -export([out_degree/2, in_degree/2]).
 
--export_type([network/0, d_type/0, neuron/0, conn/0]).
+-export_type([netgraph/0, d_type/0, neuron/0, conn/0]).
 
--record(network, {
+-record(netgraph, {
     ntab = notable   :: ets:tab(),
     ctab = notable   :: ets:tab(),
     dtab = notable   :: ets:tab(),
     rtab = norable   :: ets:tab(),
     recurrent = true :: boolean()
 }).
--type network() :: #network{}.
+-type netgraph() :: #netgraph{}.
 -type conn()    :: term().
 -type neuron()  :: term().
 -type d_type()  :: 'sequential' | 'recurrent'.
@@ -53,16 +53,16 @@
 
 %%%===================================================================
 %%% API
-%%%==================================================================
+%%%===================================================================
 
 %%-------------------------------------------------------------------
-%% @doc Creates a new network.  
+%% @doc Creates a new netgraph.  
 %% @end
 %%-------------------------------------------------------------------
--spec new() -> network().
+-spec new() -> netgraph().
 new() -> new(recurrent).
 
--spec new(Type) -> network() when
+-spec new(Type) -> netgraph() when
       Type :: [d_type()].
 new(Type) ->
     case check_type(Type, []) of
@@ -72,7 +72,7 @@ new(Type) ->
         DT = ets:new(   direct, ?DTAB_CONFIGUTATION),
         RT = ets:new(recurrent, ?RTAB_CONFIGUTATION),
         ets:insert(NT, [{'$start', 0}, {'$end', 0}]),
-        set_type(Ts, #network{ntab=NT, ctab=CT, dtab=DT, rtab=RT});
+        set_type(Ts, #netgraph{ntab=NT, ctab=CT, dtab=DT, rtab=RT});
     error ->
         erlang:error(badarg)
     end.
@@ -82,35 +82,35 @@ check_type([recurrent | Ts], L) -> check_type(Ts, [{recurrent, true} | L]);
 check_type(              [], L) -> {ok, L};
 check_type(               _, _) -> error.
 
-set_type([{recurrent,X} | Ks], NN) -> set_type(Ks, NN#network{recurrent = X});
+set_type([{recurrent,X} | Ks], NN) -> set_type(Ks, NN#netgraph{recurrent = X});
 set_type(                  [], NN) -> NN.
 
 %%-------------------------------------------------------------------
-%% @doc Deletes a network.  
+%% @doc Deletes a netgraph.  
 %% @end
 %%-------------------------------------------------------------------
 -spec delete(NN) -> 'true' when
-      NN :: network().
+      NN :: netgraph().
 delete(NN) ->
-    ets:delete(NN#network.ntab),
-    ets:delete(NN#network.ctab),
-    ets:delete(NN#network.dtab),
-    ets:delete(NN#network.rtab).
+    ets:delete(NN#netgraph.ntab),
+    ets:delete(NN#netgraph.ctab),
+    ets:delete(NN#netgraph.dtab),
+    ets:delete(NN#netgraph.rtab).
 
 %%-------------------------------------------------------------------
-%% @doc Information from the network.  
+%% @doc Information from the netgraph.  
 %% @end
 %%-------------------------------------------------------------------
 -spec info(NN) -> InfoList when
-      NN :: network(),
+      NN :: netgraph(),
       InfoList :: [{'type', Type :: d_type()} |
                    {'memory', NoWords :: non_neg_integer()}].
 info(NN) ->
-    NT = NN#network.ntab,
-    CT = NN#network.ctab,
-    DT = NN#network.dtab,
-    RT = NN#network.rtab,
-    Type = case NN#network.recurrent of
+    NT = NN#netgraph.ntab,
+    CT = NN#netgraph.ctab,
+    DT = NN#netgraph.dtab,
+    RT = NN#netgraph.rtab,
+    Type = case NN#netgraph.recurrent of
             true  -> recurrent;
             false -> sequential
         end,
@@ -120,63 +120,63 @@ info(NN) ->
     [{type, Type}, {memory, Memory}, {protection, Protection}].
 
 %%-------------------------------------------------------------------
-%% @doc Adds a neuron to the network.  
+%% @doc Adds a neuron to the netgraph.  
 %%
 %% TODO: A neuron must have at least 1 direct input
 %% TODO: How to differenciate the direct from recurrent?
 %% @end
 %%-------------------------------------------------------------------
 -spec add_neuron(NN) -> neuron() when
-      NN :: network().
+      NN :: netgraph().
 add_neuron(NN) ->
     add_neuron(NN, neuron:new()).
 
 -spec add_neuron(NN, N) -> neuron() when
-      NN :: network(),
+      NN :: netgraph(),
       N  :: neuron().
 add_neuron(NN, N) ->
-    ets:insert(NN#network.ntab, {N, []}),
+    ets:insert(NN#netgraph.ntab, {N, []}),
     N.
 
 %%-------------------------------------------------------------------
-%% @doc Deletes a neuron from the network.  
+%% @doc Deletes a neuron from the netgraph.  
 %% @end
 %%-------------------------------------------------------------------
 -spec del_neuron(NN, N) -> 'true' when
-      NN :: network(),
+      NN :: netgraph(),
       N  :: neuron().
 del_neuron(NN, N1) ->
     [del_conn(NN, N1, N2) || N2 <- out_neighbours(NN, N1)],
     [del_conn(NN, N2, N1) || N2 <-  in_neighbours(NN, N1)],
     neuron:delete(N1),
-    ets:delete(NN#network.ntab, N1).
+    ets:delete(NN#netgraph.ntab, N1).
 
 %%-------------------------------------------------------------------
-%% @doc Returns the number of neurons of the network.  
+%% @doc Returns the number of neurons of the netgraph.  
 %% @end
 %%-------------------------------------------------------------------
 -spec no_neurons(NN) -> non_neg_integer() when
-      NN :: network().
+      NN :: netgraph().
 no_neurons(NN) ->
-    ets:info(NN#network.ntab, size).
+    ets:info(NN#netgraph.ntab, size).
 
 %%-------------------------------------------------------------------
-%% @doc Returns a list of all neurons of the network.  
+%% @doc Returns a list of all neurons of the netgraph.  
 %% @end
 %%-------------------------------------------------------------------
 -spec neurons(NN) -> Neurons when
-      NN :: network(),
+      NN :: netgraph(),
       Neurons :: [neuron()].
 neurons(NN) ->
-    ets:select(NN#network.ntab, [{{'$1', '_'}, [], ['$1']}]).
+    ets:select(NN#netgraph.ntab, [{{'$1', '_'}, [], ['$1']}]).
 
 %%-------------------------------------------------------------------
-%% @doc Returns all neurons in the network which have at least one
+%% @doc Returns all neurons in the netgraph which have at least one
 %% input.  
 %% @end
 %%-------------------------------------------------------------------
 -spec sink_neurons(NN) -> Neurons when
-      NN :: network(),
+      NN :: netgraph(),
       Neurons :: [neuron()].
 sink_neurons(NN) ->
     sets:to_list(sets:from_list(
@@ -185,19 +185,19 @@ sink_neurons(NN) ->
     )).
 
 -spec sink_neurons(NN, Type) -> Neurons when
-      NN      :: network(),
+      NN      :: netgraph(),
       Type    :: d_type(),
       Neurons :: [neuron()].
 sink_neurons(NN, Type) ->
     collect_neurons(NN, Type, out).
 
 %%-------------------------------------------------------------------
-%% @doc Returns all neurons in the network which have at least one
+%% @doc Returns all neurons in the netgraph which have at least one
 %% output.  
 %% @end
 %%-------------------------------------------------------------------
 -spec source_neurons(NN) -> Neurons when
-      NN :: network(),
+      NN :: netgraph(),
       Neurons :: [neuron()].
 source_neurons(NN) ->
     sets:to_list(sets:from_list(
@@ -206,112 +206,112 @@ source_neurons(NN) ->
     )).
 
 -spec source_neurons(NN, Type) -> Neurons when
-      NN      :: network(),
+      NN      :: netgraph(),
       Type    :: d_type(),
       Neurons :: [neuron()].
 source_neurons(NN, Type) ->
     collect_neurons(NN, Type, in).
 
 %%-------------------------------------------------------------------
-%% @doc Returns the in-degree of neuron N of network NN.  
+%% @doc Returns the in-degree of neuron N of netgraph NN.  
 %% @end
 %%-------------------------------------------------------------------
 -spec in_degree(NN, N) -> non_neg_integer() when
-      NN :: network(),
+      NN :: netgraph(),
       N  :: neuron().
 in_degree(NN, N) ->
-    length(ets:lookup(NN#network.dtab, {in, N})) +
-    length(ets:lookup(NN#network.rtab, {in, N})).
+    length(ets:lookup(NN#netgraph.dtab, {in, N})) +
+    length(ets:lookup(NN#netgraph.rtab, {in, N})).
 
 %%-------------------------------------------------------------------
-%% @doc Returns the out-degree of neuron N of network NN.  
+%% @doc Returns the out-degree of neuron N of netgraph NN.  
 %% @end
 %%-------------------------------------------------------------------
 -spec out_degree(NN, N) -> non_neg_integer() when
-      NN :: network(),
+      NN :: netgraph(),
       N  :: neuron().
 out_degree(NN, N) ->
-    length(ets:lookup(NN#network.dtab, {out, N})) + 
-    length(ets:lookup(NN#network.rtab, {out, N})).
+    length(ets:lookup(NN#netgraph.dtab, {out, N})) + 
+    length(ets:lookup(NN#netgraph.rtab, {out, N})).
 
 %%-------------------------------------------------------------------
-%% @doc Returns a list of all in-neighbors of N of network NN. 
+%% @doc Returns a list of all in-neighbors of N of netgraph NN. 
 %% @end
 %%-------------------------------------------------------------------
 -spec in_neighbours(NN, N) -> Neurons when
-      NN :: network(),
+      NN :: netgraph(),
       N  :: neuron(),
       Neurons :: [neuron()].
 in_neighbours(NN, N) ->
-    CT = NN#network.ctab,
-    DT = NN#network.dtab,
-    RT = NN#network.rtab,
+    CT = NN#netgraph.ctab,
+    DT = NN#netgraph.dtab,
+    RT = NN#netgraph.rtab,
     collect_elems(ets:lookup(DT, {in, N}), CT, 2) ++ 
     collect_elems(ets:lookup(RT, {in, N}), CT, 2).
 
 %%-------------------------------------------------------------------
-%% @doc Returns a list of all out-neighbors of N of network NN. 
+%% @doc Returns a list of all out-neighbors of N of netgraph NN. 
 %% @end
 %%-------------------------------------------------------------------
 -spec out_neighbours(NN, N) -> Neurons when
-      NN :: network(),
+      NN :: netgraph(),
       N  :: neuron(),
       Neurons :: [neuron()].
 out_neighbours(NN, N) ->
-    CT = NN#network.ctab,
-    DT = NN#network.dtab,
-    RT = NN#network.rtab,
+    CT = NN#netgraph.ctab,
+    DT = NN#netgraph.dtab,
+    RT = NN#netgraph.rtab,
     collect_elems(ets:lookup(DT, {out, N}), CT, 3) ++
     collect_elems(ets:lookup(RT, {out, N}), CT, 3).
 
 %%-------------------------------------------------------------------
-%% @doc Returns all connections incident on N of network NN. 
+%% @doc Returns all connections incident on N of netgraph NN. 
 %% @end
 %%-------------------------------------------------------------------
 -spec in_conn(NN, N) -> Connections when
-      NN :: network(),
+      NN :: netgraph(),
       N  :: neuron(),
       Connections :: [conn()].
 in_conn(NN, N) ->
     in_conn(NN, N, sequential) ++ in_conn(NN, N, recurrent).
 
 -spec in_conn(NN, N, Type) -> Connections when
-      NN   :: network(),
+      NN   :: netgraph(),
       N    :: neuron(),
       Type :: d_type(),
       Connections :: [conn()].
 in_conn(NN, N, sequential) -> 
-    ets:select(NN#network.dtab,[{{{ in,N},'$1'},[],['$1']}]);
+    ets:select(NN#netgraph.dtab,[{{{ in,N},'$1'},[],['$1']}]);
 in_conn(NN, N, recurrent)  -> 
-    ets:select(NN#network.rtab,[{{{ in,N},'$1'},[],['$1']}]).
+    ets:select(NN#netgraph.rtab,[{{{ in,N},'$1'},[],['$1']}]).
 
 %%-------------------------------------------------------------------
-%% @doc Returns all connections emanating from N of network NN. 
+%% @doc Returns all connections emanating from N of netgraph NN. 
 %% @end
 %%-------------------------------------------------------------------
 -spec out_conn(NN, N) -> Connections when
-      NN :: network(),
+      NN :: netgraph(),
       N  :: neuron(),
       Connections :: [conn()].
 out_conn(NN, N) ->
     out_conn(NN, N, sequential) ++ out_conn(NN, N, recurrent).
 
 -spec out_conn(NN, N, Type) -> Connections when
-      NN   :: network(),
+      NN   :: netgraph(),
       N    :: neuron(),
       Type :: d_type(),
       Connections :: [conn()].
 out_conn(NN, N, sequential) ->
-    ets:select(NN#network.dtab,[{{{out,N},'$1'},[],['$1']}]);
+    ets:select(NN#netgraph.dtab,[{{{out,N},'$1'},[],['$1']}]);
 out_conn(NN, N, recurrent) -> 
-    ets:select(NN#network.rtab,[{{{out,N},'$1'},[],['$1']}]).
+    ets:select(NN#netgraph.rtab,[{{{out,N},'$1'},[],['$1']}]).
 
 %%-------------------------------------------------------------------
 %% @doc Creates (or modifies) a connection between N1 and N2. 
 %% @end
 %%------------------------------------------------------------------
 -spec add_conn(NN, N1, N2) -> Result when
-      NN :: network(),
+      NN :: netgraph(),
       N1 :: neuron(),
       N2 :: neuron(),
       Result :: conn() | {'error', add_conn_err_rsn()}.
@@ -329,15 +329,15 @@ insert_conn(NN, N1, N2) ->
         Path  -> insert_rcc_conn(NN, N1, N2, Path)
     end.
 
-insert_seq_conn(#network{dtab=DT, ctab=CT}, N1, N2) ->
+insert_seq_conn(#netgraph{dtab=DT, ctab=CT}, N1, N2) ->
     Id = connection:new(N1, N2),
     ets:insert(DT, [{{out, N1}, Id}, {{in, N2}, Id}]),
     ets:insert(CT, {Id, N1, N2, []}),
     Id.
 
-insert_rcc_conn(#network{recurrent=false}, _, _, Path) ->
+insert_rcc_conn(#netgraph{recurrent=false}, _, _, Path) ->
     {error, {bad_conn, Path}};
-insert_rcc_conn(#network{rtab=RT, ctab=CT}, N1, N2, _) ->
+insert_rcc_conn(#netgraph{rtab=RT, ctab=CT}, N1, N2, _) ->
     Id = connection:new(N1, N2),
     ets:insert(RT, [{{out, N1}, Id}, {{in, N2}, Id}]),
     ets:insert(CT, {Id, N1, N2, []}),
@@ -348,47 +348,47 @@ insert_rcc_conn(#network{rtab=RT, ctab=CT}, N1, N2, _) ->
 %% @end
 %%-------------------------------------------------------------------
 -spec del_conn(NN, N1, N2) -> 'true' when
-      NN :: network(),
+      NN :: netgraph(),
       N1 :: neuron(),
       N2 :: neuron().
 del_conn(NN, N1, N2) ->
     C = connection:id(N1, N2),
     DQuery = [{{{in,N2},C}, [], [true]}, {{{out,N1},C}, [], [true]}],
-    ets:select_delete(NN#network.dtab, DQuery),
-    ets:select_delete(NN#network.rtab, DQuery),
-    ets:delete(NN#network.ctab, C).
+    ets:select_delete(NN#netgraph.dtab, DQuery),
+    ets:select_delete(NN#netgraph.rtab, DQuery),
+    ets:delete(NN#netgraph.ctab, C).
 
 %%-------------------------------------------------------------------
-%% @doc Returs the number of connections in the network. 
+%% @doc Returs the number of connections in the netgraph. 
 %% @end
 %%-------------------------------------------------------------------
 -spec no_conn(NN) -> non_neg_integer() when
-      NN :: network().
+      NN :: netgraph().
 no_conn(NN) ->
-    ets:info(NN#network.ctab, size).
+    ets:info(NN#netgraph.ctab, size).
 
 %%-------------------------------------------------------------------
-%% @doc Returs all network connections. 
+%% @doc Returs all netgraph connections. 
 %% @end
 %%-------------------------------------------------------------------
 -spec conn(NN) -> Conns when
-      NN :: network(),
+      NN :: netgraph(),
       Conns :: [conn()].
 conn(NN) ->
-    ets:select(NN#network.ctab, [{{'$1','_','_','_'}, [], ['$1']}]).
+    ets:select(NN#netgraph.ctab, [{{'$1','_','_','_'}, [], ['$1']}]).
 
 %%-------------------------------------------------------------------
 %% @doc Returs all the neuron connections. 
 %% @end
 %%-------------------------------------------------------------------
 -spec conn(NN, N) -> Conns when
-      NN :: network(),
+      NN :: netgraph(),
       N  :: neuron(),
       Conns :: [conn()].
 conn(NN, N) ->
     Query = [{{{out,N},'$1'},[],['$1']}, {{{in,N},'$1'},[],['$1']}],
-    ets:select(NN#network.dtab, Query) ++ 
-    ets:select(NN#network.rtab, Query).
+    ets:select(NN#netgraph.dtab, Query) ++ 
+    ets:select(NN#netgraph.rtab, Query).
 
 
 %%====================================================================
@@ -397,7 +397,7 @@ conn(NN, N) ->
 
 % Cheks if the neuron belongs to the ntab ----------------------------
 check_neurons(NN, [N | Neurons]) -> 
-    case ets:member(NN#network.ntab, N) of
+    case ets:member(NN#netgraph.ntab, N) of
         false -> {error, {bad_neuron, N}};
         true  -> check_neurons(NN, Neurons)
     end;
@@ -408,8 +408,8 @@ check_neurons(_NN, []) ->
 collect_neurons(NN, Type, Direction) -> 
     Ns = neurons(NN),
     case Type of 
-        sequential -> filter_neurons(Ns, NN#network.dtab, Direction);
-        recurrent  -> filter_neurons(Ns, NN#network.rtab, Direction)
+        sequential -> filter_neurons(Ns, NN#netgraph.dtab, Direction);
+        recurrent  -> filter_neurons(Ns, NN#netgraph.rtab, Direction)
     end.
 
 % Filters the neurons with a specific direction from a table --------
