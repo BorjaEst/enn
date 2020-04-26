f887c62a096aa7408841052172d72d99b0001379
BorjaEst <boressan@outlook.com>
2020-04-26 09:36:50 +0200

    New suite to test the correct saving of results

---
+++ test/enn_save_SUITE.erl
@@ -0,0 +1,180 @@
+%%%-------------------------------------------------------------------
+%%% Author  :
+%%% Description :
+%%% Created :
+%%%-------------------------------------------------------------------
+-module(enn_save_SUITE).
+-compile([export_all, nowarn_export_all]).
+
+-include_lib("common_test/include/ct.hrl").
+
+-define(HEAD, [$- || _ <-lists:seq(1,80)] ++ "\n").
+-define(HEAD(Text), ct:log(?LOW_IMPORTANCE, ?HEAD ++ "~p", [Text])).
+-define(END,  [$_ || _ <-lists:seq(1,80)]).
+-define(END(V), ct:log(?LOW_IMPORTANCE, "~p~n" ++ ?END, [V]), V).
+
+-define(INFO(A,B),    ct:log(?LOW_IMPORTANCE,    "~p: ~p",   [A,B])).
+-define(ERROR(Error), ct:pal( ?HI_IMPORTANCE, "Error: ~p", [Error])).
+
+
+%%--------------------------------------------------------------------
+%% Function: suite() -> Info
+%% Info = [tuple()]
+%%--------------------------------------------------------------------
+suite() ->
+    [{timetrap, {seconds, 8}}].
+
+%%--------------------------------------------------------------------
+%% Function: init_per_suite(Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%%--------------------------------------------------------------------
+init_per_suite(Config) ->
+    application:start(enn),
+    Config.
+
+%%--------------------------------------------------------------------
+%% Function: end_per_suite(Config0) -> term() | {save_config,Config1}
+%% Config0 = Config1 = [tuple()]
+%%--------------------------------------------------------------------
+end_per_suite(_Config) ->
+    application:stop(enn),
+    ok.
+
+%%--------------------------------------------------------------------
+%% Function: init_per_group(GroupName, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% GroupName = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%%--------------------------------------------------------------------
+init_per_group(_GroupName, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% Function: end_per_group(GroupName, Config0) ->
+%%               term() | {save_config,Config1}
+%% GroupName = atom()
+%% Config0 = Config1 = [tuple()]
+%%--------------------------------------------------------------------
+end_per_group(_GroupName, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% Function: init_per_testcase(TestCase, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% TestCase = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%%--------------------------------------------------------------------
+init_per_testcase(_GroupName, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% Function: end_per_testcase(TestCase, Config0) ->
+%%               term() | {save_config,Config1} | {fail,Reason}
+%% TestCase = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%%--------------------------------------------------------------------
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% Function: groups() -> [Group]
+%% Group = {GroupName,Properties,GroupsAndTestCases}
+%% GroupName = atom()
+%% Properties = [parallel | sequence | Shuffle | {RepeatType,N}]
+%% GroupsAndTestCases = [Group | {group,GroupName} | TestCase]
+%% TestCase = atom()
+%% Shuffle = shuffle | {shuffle,{integer(),integer(),integer()}}
+%% RepeatType = repeat | repeat_until_all_ok | repeat_until_all_fail |
+%%              repeat_until_any_ok | repeat_until_any_fail
+%% N = integer() | forever
+%%--------------------------------------------------------------------
+groups() ->
+    [ ].
+
+%%--------------------------------------------------------------------
+%% Function: all() -> GroupsAndTestCases | {skip,Reason}
+%% GroupsAndTestCases = [{group,GroupName} | TestCase]
+%% GroupName = atom()
+%% TestCase = atom()
+%% Reason = term()
+%%--------------------------------------------------------------------
+all() ->
+    [ 
+        training_saved_after_stop,
+        correct_network_clonation
+    ].
+
+%%--------------------------------------------------------------------
+%% Function: TestCase() -> Info
+%% Info = [tuple()]
+%%--------------------------------------------------------------------
+my_test_case_example() ->
+    [].
+
+%%--------------------------------------------------------------------
+%% Function: TestCase(Config0) ->
+%%               ok | exit() | {skip,Reason} | {comment,Comment} |
+%%               {save_config,Config1} | {skip_and_save,Reason,Config1}
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% Comment = term()
+%%--------------------------------------------------------------------
+my_test_case_example(_Config) ->
+    ok.
+
+% --------------------------------------------------------------------
+% TESTS --------------------------------------------------------------
+
+% -------------------------------------------------------------------
+training_saved_after_stop() ->
+    [].
+training_saved_after_stop(_Config) ->
+    ?HEAD("Correct neural network saving after stop .............."),
+    Id = enn:start(test_architectures:example()),
+    Ns = neurons(Id),
+    Ls = links(Id),
+    {In,Opt} = test_data_generators:random_sum_of_inputs(
+                                  enn:inputs(Id),enn:outputs(Id),10),
+    _  = enn:fit(Id, In, Opt),
+    ok = enn:stop(Id),
+    true = Ns /= neurons(Id),
+    true = Ls /=   links(Id),
+    ?END(ok).
+
+% -------------------------------------------------------------------
+correct_network_clonation() -> 
+    [].
+correct_network_clonation(_Config) -> 
+    ?HEAD("Network can be cloned ................................."),
+    Id1 = enn:compile(test_architectures:example()),
+    Id2 = enn:clone(Id1),
+    true = neurons(Id1) /= neurons(Id2),
+    true =   links(Id1) /=   links(Id2),
+    ?END(ok).
+
+
+% --------------------------------------------------------------------
+% SPECIFIC HELPER FUNCTIONS ------------------------------------------
+
+% Gets the network id neurons ---------------------------------------
+neurons(Network_id) -> 
+    NN  = edb:read(Network_id),
+    Ids = network:neurons(NN),
+    edb:read(Ids). 
+
+% Gets the network id links -----------------------------------------
+links(Network_id) -> 
+    NN  = edb:read(Network_id),
+    Ids = network:links(NN),
+    edb:read(Ids). 
+
+
+% --------------------------------------------------------------------
+% RESULTS CONSOLE PRINT ----------------------------------------------
+
+
